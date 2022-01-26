eda204dfe720056624abe25d55e308d230c9a472
RobeSantoro <santoro.robe@gmail.com>
2022-01-26 22:36:03 +0100

    Better Structure amd Images

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-import './style.css'
 
 import * as THREE from 'three'
 import Stats from 'three/examples/jsm/libs/stats.module.js'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+// Import GLTF and DRACO loader
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
 
-// Window. Sizes
-const sizes = {
-  height: window.innerHeight,
-  width: window.innerWidth
-}
+/****************************************************************************************/
+/*********************************************************************** THREE.JS SETUP */
+/****************************************************************************************/
 
 // Create a scene
 const scene = new THREE.Scene()
@@ -16,20 +16,23 @@ const scene = new THREE.Scene()
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
+// Window. Sizes
+const sizes = {
+  height: window.innerHeight,
+  width: window.innerWidth
+}
+
 // Create a camera
-const camera = new THREE.PerspectiveCamera(  25,  sizes.width / sizes.height,  0.1,  1000)
-camera.position.z = 5
+const camera = new THREE.PerspectiveCamera(25, sizes.width / sizes.height, 0.1, 1000)
+camera.position.set(0, 2, 5)
 scene.add(camera)
 
 // Orbit controls
 const controls = new OrbitControls(camera, canvas)
+// Set the lookat of the OrbitControls
+controls.target.set(0, 0, 0)
 controls.dampingFactor = 0.25
-controls.enableDamping = true
-controls.enableZoom = true
-controls.enablePan = true
-controls.enableRotate = true
 controls.autoRotate = true
-controls.autoRotateSpeed = 10
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
@@ -37,35 +40,58 @@ const renderer = new THREE.WebGLRenderer({
   antialias: true,
   alpha: false,
   preserveDrawingBuffer: false,
-  premultipliedAlpha: false })
+  premultipliedAlpha: false
+})
+
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(devicePixelRatio, 2))
 renderer.setClearColor(0x101010, 0)
 renderer.render(scene, camera)
+renderer.outputEncoding = THREE.sRGBEncoding
 
+/*****************************************************************************************/
+/********************************************************************* ENVIRONMENT SETUP */
+/*****************************************************************************************/
 
 // Load the environment texture
 const textureLoader = new THREE.TextureLoader()
-const envTexture = textureLoader.load('./assets/garage_1k.jpg')
-envTexture.mapping = THREE.EquirectangularReflectionMapping
+const envMap = textureLoader.load('./textures/envMap.jpg')
+envMap.mapping = THREE.EquirectangularReflectionMapping
+envMap.encoding = THREE.sRGBEncoding
 
-// Create a cube
+// Create a ground plane
 const geometry = new THREE.BoxBufferGeometry(1, 1, 1)
 const material = new THREE.MeshStandardMaterial({
   color: 0xffffff,
-  envMap: envTexture,
+  envMap: envMap,
   metalness: 1,
-  roughness: 0.02,
+  roughness: 0.05,
   envMapIntensity: 1,
-  side: THREE.DoubleSide })
+  side: THREE.DoubleSide
+})
 const cube = new THREE.Mesh(geometry, material)
 scene.add(cube)
+
+
+/*****************************************************************************************/
+/************************************************************************* TEXTURE SETUP */
+/*****************************************************************************************/
+
+
+
+
+/*****************************************************************************************/
+/*************************************************************************** LIGHTS ******/
+/*****************************************************************************************/
 
 // Create a light
 const light = new THREE.PointLight(0xffffff, 1, 100)
 light.position.set(10, 10, 10)
 scene.add(light)
 
+/*****************************************************************************************/
+/************************************************************* CLOCK TICK AND STATS ******/
+/*****************************************************************************************/
 
 // Initialize the main loop
 const clock = new THREE.Clock()
@@ -77,35 +103,33 @@ const stats = new Stats()
 stats.showPanel(0)
 document.body.appendChild(stats.dom)
 
-// Create the main loop invoking the animate function
-const animate = () =>
-{
+// Create the main loop invoking the tick function
+const tick = () => {
   const elapsedTime = clock.getElapsedTime()
   const deltaTime = elapsedTime - lastElapsedTime
   lastElapsedTime = elapsedTime
 
   //FPS
-  FPS = Math.round(1/deltaTime)
-  
+  FPS = Math.round(1 / deltaTime)
+
   // Update controls
   controls.update()
-  
+
   // Update stats
   stats.update()
 
   // Render
-  renderer.render(scene, camera)    
+  renderer.render(scene, camera)
 
-  // Call animate again on the next frame
-  requestAnimationFrame(animate)
+  // Call tick again on the next frame
+  requestAnimationFrame(tick)
 }
 
-animate()
-
+tick()
 
 // Manage the resize of the window
-addEventListener('resize', () => {  
-  
+addEventListener('resize', () => {
+
   sizes.width = innerWidth
   sizes.height = innerHeight
   camera.aspect = sizes.width / sizes.height
@@ -113,5 +137,5 @@ addEventListener('resize', () => {
   renderer.setSize(sizes.width, sizes.height)
   renderer.setPixelRatio(Math.min(devicePixelRatio, 2))
   renderer.render(scene, camera)
-  
+
 })
