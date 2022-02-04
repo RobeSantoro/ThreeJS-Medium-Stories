d38ddf2a6f32e837679b166d7511a37592fd6a53
RobeSantoro <santoro.robe@gmail.com>
2022-02-04 22:48:23 +0100

    Switched to StdMaterial, Added Dir Light

--- main.js
+++ main.js
@@ -6,20 +6,28 @@ const canvas = document.querySelector('canvas.webgl')
 // Create the Scene
 const scene = new THREE.Scene()
 
-// Create the Camera
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-camera.position.z = 5
+// Camera
+const camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 0.1, 1000)
+camera.position.set(-5, 3, 5)
+camera.lookAt(new THREE.Vector3(0, 0, 0))
+
+scene.add(camera)
 
 // Create a Cube
 const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const material = new THREE.MeshStandardMaterial({ color: 0x00ff00 })
 const cube = new THREE.Mesh(geometry, material)
 
 scene.add(cube)
 
-// Create the Renderer
-const renderer = new THREE.WebGLRenderer({ canvas: canvas})
+// Add a light
+const light = new THREE.DirectionalLight(0xffffff, 1)
+light.position.set(-1, 3, 4)
 
+scene.add(light)
+
+// Create the Renderer
+const renderer = new THREE.WebGLRenderer( { canvas : canvas } )
 // Render the Scene
 renderer.setSize(window.innerWidth, window.innerHeight)
 renderer.render(scene, camera)
